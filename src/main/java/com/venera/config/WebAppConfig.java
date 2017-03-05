1577377f33c343c005ad35bb24667e120521e986
KingAlexJ8 <KingAlexJ8@ukr.net>
2017-03-05 14:49:28 -0800

    WebAppConfig

--- src/main/java/com/venera/config/WebAppConfig.java
+++ src/main/java/com/venera/config/WebAppConfig.java
@@ -1,7 +1,34 @@
-package main.java.com.venera.config;
+package com.venera.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by Oleh Hershtun on 3/5/2017.
  */
-public class WebAppConfig {
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.venera")
+public class WebAppConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers (ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver (){
+        InternalResourceViewResolver resolver =  new InternalResourceViewResolver();
+        resolver.setPrefix("/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
 }
